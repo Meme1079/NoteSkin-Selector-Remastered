9d818e280ed5b795da850346219864c58b634ead
Meme1079 <101881784+Meme1079@users.noreply.github.com>
2026-03-07 11:09:44 +0800

    made the editor notes an object

--- api/classes/editor/notes/EditorNotes.lua
+++ api/classes/editor/notes/EditorNotes.lua
@@ -1,4 +1,83 @@
+local F = require 'mods.NoteSkin Selector Remastered.api.libraries.f-strings.F'
+
+local SKIN_DIRECTIONS = {'left', 'down', 'up', 'right'}
+local SKIN_COLORS     = {'purple0', 'blue0', 'green0', 'red0'}
+
+---@class EditorNotes
 local EditorNotes = {}
+
+---
+---@param tag string
+---@param sprite string
+---@return SkinNotes
+function EditorNotes:new(tag, sprite)
+     local self = setmetatable({}, {__index = self})
+     self.tag    = tag
+     self.sprite = sprite
+
+     return self
+end
+
+
+function EditorNotes:create(strum, x, y, size)
+     local ddoodoo = self:tag_strums(strum)
+
+     makeAnimatedLuaSprite(ddoodoo, 'noteSkins/NOTE_assets-DSides', x, y)
+     scaleObject(ddoodoo, size[1], size[2])
+     addAnimationByPrefix(ddoodoo, F"{SKIN_DIRECTIONS[strum]}_confirm", F"{SKIN_DIRECTIONS[strum]} confirm", 24, false)
+     addAnimationByPrefix(ddoodoo, F"{SKIN_DIRECTIONS[strum]}_pressed", F"{SKIN_DIRECTIONS[strum]} pressed", 24, false)
+     addAnimationByPrefix(ddoodoo, F"{SKIN_DIRECTIONS[strum]}_colored", SKIN_COLORS[strum], 24, false)
+     addAnimationByPrefix(ddoodoo, SKIN_DIRECTIONS[strum], F"arrow{SKIN_DIRECTIONS[strum]:upper()}", 24, false)
+     playAnim(ddoodoo, SKIN_DIRECTIONS[strum], false)
+     setObjectCamera(ddoodoo, 'camHUD')
+     addLuaSprite(ddoodoo)
+end
+
+local dir = 1
+
+local dx, dy = 0, 0 -- Directional input variables
+local di = 1        -- Amplifier
+function EditorNotes:update_movement()
+     if keyboardPressed('D') then dx = dx + 1 end
+     if keyboardPressed('A') then dx = dx - 1 end
+     if keyboardPressed('S') then dy = dy + 1 end
+     if keyboardPressed('W') then dy = dy - 1 end
+
+     local giX = F"{self.tag}{dir}"
+
+     debugPrint(giX)
+
+     local length = math.sqrt(dx^2 + dy^2)
+     if length > 0 then
+          dx = dx / length
+          dy = dy / length
+
+          if keyboardPressed('D') or keyboardPressed('A') and not (keyboardPressed('D') and keyboardPressed('A')) then
+               setProperty(F"{giX}.x", getProperty(F"{giX}.x") + dx*di)
+          end
+          if keyboardPressed('S') or keyboardPressed('W') and not (keyboardPressed('S') and keyboardPressed('W')) then
+               setProperty(F"{giX}.y", getProperty(F"{giX}.y") + dy*di)
+          end
+          setTextString('animationEditorStrumsInput', math.round(getProperty(F"{giX}.x"), 2))
+     end
+
+     if keyboardJustPressed('LBRACKET') and dir > 1 then
+          dir = dir - 1
+          setTextString('animationEditorStrumsInput', math.round(getProperty(F"{giX}.x"), 2))
+     end
+     if keyboardJustPressed('RBRACKET') and dir < 4 then
+          dir = dir + 1
+          setTextString('animationEditorStrumsInput', math.round(getProperty(F"{giX}.x"), 2))
+     end
+end
+
+
+function EditorNotes:set_sprite(sprite)
+end
+
+function EditorNotes:tag_strums(strum)
+     return self.tag..tostring(strum)
+end
 
 -- TODO:
 -- inititating the noteskin to adjust the offsets for preview
--- data/skin-editor/test.lua
+++ data/skin-editor/test.lua
@@ -1,4 +1,73 @@
-local FlavorUI_Toggle = require 'mods.NoteSkin Selector Remastered.api.classes.ui.FlavorUI_Toggle'
+local EditorNotes = require 'mods.NoteSkin Selector Remastered.api.classes.editor.notes.EditorNotes'
+
+local F    = require 'mods.NoteSkin Selector Remastered.api.libraries.f-strings.F'
+local math = require 'mods.NoteSkin Selector Remastered.api.libraries.standard.math'
+
+local fart = EditorNotes:new('fartdoodoo', 'noteSkins/NOTE_assets-DSides')
+fart:create(1, 410 + (105*(1 - 1)), 100, {0.65, 0.65})
+fart:create(2, 410 + (105*(2 - 1)), 100, {0.65, 0.65})
+fart:create(3, 410 + (105*(3 - 1)), 100, {0.65, 0.65})
+fart:create(4, 410 + (105*(4 - 1)), 100, {0.65, 0.65})
+
+function onUpdatePost(elapsed)
+     fart:update_movement()
+end
+
+
+--[[ local directions = {'left', 'down', 'up', 'right'}
+local colors = {'purple0', 'blue0', 'green0', 'red0'}
+for strumIndex = 1, 4 do
+     local awesomeTag = F"notesTesta{strumIndex}"
+     makeAnimatedLuaSprite(awesomeTag, 'noteSkins/NOTE_assets-DSides', 410 + (105*(strumIndex - 1)), 100)
+     scaleObject(awesomeTag, 0.65, 0.65)
+     addAnimationByPrefix(awesomeTag, F"{directions[strumIndex]}_confirm", F"{directions[strumIndex]} confirm", 24, false)
+     addAnimationByPrefix(awesomeTag, F"{directions[strumIndex]}_pressed", F"{directions[strumIndex]} pressed", 24, false)
+     addAnimationByPrefix(awesomeTag, F"{directions[strumIndex]}_colored", colors[stumIndex], 24, false)
+     addAnimationByPrefix(awesomeTag, directions[strumIndex], F"arrow{directions[strumIndex]:upper()}", 24, false)
+     playAnim(awesomeTag, directions[strumIndex], false)
+     setObjectCamera(awesomeTag, 'camHUD')
+     addLuaSprite(awesomeTag)
+end
+
+local dir = 1
+
+local dx, dy = 0, 0 -- Directional input variables
+local di = 1        -- Amplifier
+function onUpdatePost(elapsed)
+     if keyboardPressed('D') then dx = dx + 1 end
+     if keyboardPressed('A') then dx = dx - 1 end
+     if keyboardPressed('S') then dy = dy + 1 end
+     if keyboardPressed('W') then dy = dy - 1 end
+
+     local giX = F"notesTesta{dir}"
+
+     local length = math.sqrt(dx^2 + dy^2)
+     if length > 0 then
+          dx = dx / length
+          dy = dy / length
+
+          if keyboardPressed('D') or keyboardPressed('A') and not (keyboardPressed('D') and keyboardPressed('A')) then
+               setProperty(F"{giX}.x", getProperty(F"{giX}.x") + dx*di)
+          end
+          if keyboardPressed('S') or keyboardPressed('W') and not (keyboardPressed('S') and keyboardPressed('W')) then
+               setProperty(F"{giX}.y", getProperty(F"{giX}.y") + dy*di)
+          end
+          setTextString('animationEditorStrumsInput', math.round(getProperty(F"{giX}.x"), 2))
+     end
+
+     if keyboardJustPressed('LBRACKET') and dir > 1 then
+          dir = dir - 1
+          setTextString('animationEditorStrumsInput', math.round(getProperty(F"{giX}.x"), 2))
+     end
+     if keyboardJustPressed('RBRACKET') and dir < 4 then
+          dir = dir + 1
+          setTextString('animationEditorStrumsInput', math.round(getProperty(F"{giX}.x"), 2))
+     end
+end ]]
+
+
+
+--[[ local FlavorUI_Toggle = require 'mods.NoteSkin Selector Remastered.api.classes.ui.FlavorUI_Toggle'
 local SkinSaves       = require 'mods.NoteSkin Selector Remastered.api.classes.skins.static.SkinSaves'
 
 local F         = require 'mods.NoteSkin Selector Remastered.api.libraries.f-strings.F'
@@ -33,3 +102,4 @@ end
 function onUpdatePost(elapsed)
      doodoo:update()
 end
+ ]]
